6b622be67bb43520c91ce181a3e22198b17fcc1d
Maciek Sobiecki <sobiecki.maciek@gmail.com>
2023-03-29 22:17:09 +0100

    Added heroku mySQL DB

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 import mysql2 from "mysql2"
 
 export const db = mysql2.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "sapapp"
+    host: "eu-cdbr-west-03.cleardb.net",
+    user: "b4f55b53e87fad",
+    password: "33eeacaa",
+    database: "heroku_457f3647e6d6a8e"
 })
