389292eb69a4fd6a9f3facd59ce35228baccc92f
Maciek Sobiecki <sobiecki.maciek@gmail.com>
2023-03-29 23:14:56 +0100

    Adding an error catch function in case of lost connection to the database

--- db.js
+++ db.js
@@ -6,3 +6,26 @@ export const db = mysql2.createConnection({
     password: "33eeacaa",
     database: "heroku_457f3647e6d6a8e"
 })
+
+function handleDisconnect() {
+    db.connect((err) => {
+        if (err) {
+            console.log('Error connecting to database', err)
+            setTimeout(handleDisconnect, 2000)
+        } else {
+            console.log('Connected to Database')
+        }
+    });
+
+    db.on('error', (err) => {
+        console.log('Database error', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST'){
+            console.log('Attempting to reconnect to database...')
+            handleDisconnect()
+        }else{
+            throw(err)
+        }
+    })
+}
+
+handleDisconnect()
