3dfc414fcb68452a2632df678b9011af385eafed
Maciek Sobiecki <sobiecki.maciek@gmail.com>
2023-03-30 00:20:59 +0100

    Fixing issue in db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 import mysql2 from "mysql2";
 
-const db = mysql2.createConnection({
+ export const db = mysql2.createConnection({
   host: "eu-cdbr-west-03.cleardb.net",
   user: "b4f55b53e87fad",
   password: "33eeacaa",
