71d415c9b624416e199b8cb4a514eff0f59c6e1a
Maciek Sobiecki <sobiecki.maciek@gmail.com>
2023-03-29 23:39:43 +0100

    fixing mistakes in db.js

--- db.js
+++ db.js
@@ -1,22 +1,21 @@
-import mysql2 from "mysql2"
+import mysql2 from "mysql2";
 
 export const db = mysql2.createConnection({
     host: "eu-cdbr-west-03.cleardb.net",
     user: "b4f55b53e87fad",
     password: "33eeacaa",
     database: "heroku_457f3647e6d6a8e"
-})
-
+});
 
 let lastErrorTime = null;
-function handleDatabaseError() {
+
+function handleDatabaseError(error) {
     const currentTime = Date.now();
 
     if (lastErrorTime === null || currentTime - lastErrorTime > 5000) {
         console.error(`Error connecting to database: ${error}`);
         lastErrorTime = currentTime;
     }
-
 }
 
 db.on('error', handleDatabaseError);
