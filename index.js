cff8c7444dbdc3a2edc31abe91eca723932e6698
Maciek Sobiecki <sobiecki.maciek@gmail.com>
2023-03-30 16:08:13 +0100

    Making it more secure

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ app.use(helmet.contentSecurityPolicy({
     //CWE ID 693 Passive (10038 - Content Security Policy (CSP) Header Not Set)
     directives: {
         defaultSrc: ["'self'"],
-        scriptSrc: ["'self'", "'unsafe-inline'"],
-        styleSrc: ["'self'", "'unsafe-inline'"],
+        scriptSrc: ["'self'"],
+        styleSrc: ["'self'"],
         imgSrc: ["'self'"],
         connectSrc: ["'self'"],
         fontSrc: ["'self'"],
         objectSrc: ["'none'"],
-        mediaSrc: ["'self'"]
-
+        mediaSrc: ["'self'"],
+        upgradeInsecureRequests: [], // Passive (10052 - HTTP Strict Transport Security (HSTS) Not Implemented)
     },
     // Passive (10021 - X-Content-Type-Options Header Missing) CWE 693
     contentSecurityPolicy: false,
@@ -46,8 +46,11 @@ app.use(helmet.contentSecurityPolicy({
 
 app.use(function (req, res, next) {
     //Passive (10098 - Cross-Domain Misconfiguration) CWE ID 264
+    res.header("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
     res.header("Access-Control-Allow-Origin", "https://warm-strudel-d45ad5.netlify.app"); // update the domain name
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Permissions-Policy", "camera=(), microphone=(), geolocation=()");
+    res.setHeader('X-Content-Type-Options', 'nosniff');
     res.header("Access-Control-Max-Age", "86400");
     //Passive (10020 - Anti-clickjacking Header) CWE ID 1021
     res.setHeader('X-Frame-Options', 'DENY');
